1e9b030826e5484489c87931ad69de50d5fc9fc0
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-18 14:52:47 +0900

    [Feat] 시작하기 버튼 액션 구현

---
+++ UDGame/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  UDGame
+//
+//  Created by Lee on 7/18/25.
+//
+
+import UIKit
+
+class GameViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
--- UDGame/ViewController.swift
+++ UDGame/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var logoLabel: UILabel!
+    @IBOutlet var gameLabel: UILabel!
+
+    @IBOutlet var startButton: CustomButton!
+
+    @IBOutlet var homeImageView: UIImageView!
+    @IBOutlet var numberTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigation()
     }
 
+    private func setupNavigation() {
+        navigationController?.navigationBar.isHidden = true
+    }
 
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: String(describing: "GameViewController"))
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
