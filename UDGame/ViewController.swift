c8a156de3150a4a0ba947dc53973fa4525d53a0b
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-18 13:42:28 +0900

    Initial Commit

---
+++ UDGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDGame
+//
+//  Created by Lee on 7/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
